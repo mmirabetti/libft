d00a0e3ade21273ba122d70a43c8138079e0828f
mmirabet <mmirabet@student.42sp.org.br>
2020-01-28 19:46:25 -0300

    added ft_lstdelone.c, modified Makefile and libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mmirabet <mmirabet@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 16:26:52 by mmirabet          #+#    #+#             */
-/*   Updated: 2020/01/28 17:13:13 by mmirabet         ###   ########.fr       */
+/*   Updated: 2020/01/28 19:36:32 by mmirabet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,4 +65,5 @@ void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void*));
 #endif
