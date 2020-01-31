5998b99b6c3e17c389857747995b5daa31a7506a
mmirabet <mmirabet@student.42sp.org.br>
2020-01-31 10:32:23 -0300

    fixed ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mmirabet <mmirabet@student.42sp.o...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/24 11:33:31 by mmirabet          #+#    #+#             */
-/*   Updated: 2020/01/24 12:40:36 by mmirabet         ###   ########.fr       */
+/*   Updated: 2020/01/31 10:30:42 by mmirabet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!(ptr = malloc((len + 1) * sizeof(char))))
 		return (NULL);
 	i = 0;
-	while (i < len)
+	while (i < len && s[i])
 	{
 		ptr[i] = s[start + i];
 		i++;
