521e106f69ae13c905ef400956a1bab7c56a2fa0
mauricio <mauricio@g73jh.anubisconsulting.com>
2020-02-03 15:21:33 -0300

    fixed ft_substr, when start is outside len(s)

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mmirabet <mmirabet@student.42sp.o...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/24 11:33:31 by mmirabet          #+#    #+#             */
-/*   Updated: 2020/01/31 10:30:42 by mmirabet         ###   ########.fr       */
+/*   Updated: 2020/02/03 15:19:59 by mauricio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*ptr;
 	size_t	i;
 
-	if (!s)
+	if (!s || !(ptr = malloc((len + 1) * sizeof(char))))
 		return (NULL);
-	if (!(ptr = malloc((len + 1) * sizeof(char))))
-		return (NULL);
-	i = 0;
-	while (i < len && s[i])
+	if (start >= ft_strlen(s)) 
+		*ptr = '\0';
+	else
 	{
-		ptr[i] = s[start + i];
-		i++;
+		i = 0;
+		while (i < len && s[i])
+		{
+			ptr[i] = s[start + i];
+			i++;
+		}
+		ptr[len] = '\0';
 	}
-	ptr[len] = '\0';
 	return (ptr);
 }
